d6356702edcdba46fdb16a0088c0824da55a40a0
NikiKentr <nikikentr@gmail.com>
2024-06-17 19:50:16 +0200

    with publishers

---
+++ Ros2RhinoUnity/Assets/Scripts/KeyboardControl5.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Robotics.ROSTCPConnector;
+using RosMessageTypes.Geometry;
+using RosMessageTypes.Std;
+using RosMessageTypes.BuiltinInterfaces;
+
+public class KeyboardControl5 : MonoBehaviour
+{
+    public ArticulationWheelController wheelController;
+
+    public float speed = 1.5f;
+    public float angularSpeed = 1.5f;
+    private float targetLinearSpeed;
+    private float targetAngularSpeed;
+
+    public Transform point2; // Reference to the second point
+
+    public GameObject robotBase; // Reference to the GameObject representing the robot's base
+
+    private ROSConnection ros;
+    public string poseTopic = "unity/robot2_pose"; // The ROS topic to publish pose messages for the second robot
+
+    void Start()
+    {
+        ros = ROSConnection.GetOrCreateInstance();
+        ros.RegisterPublisher<PoseStampedMsg>(poseTopic);
+    }
+
+    void Update()
+    {
+        // Get key input
+        targetLinearSpeed = Input.GetAxisRaw("Vertical2") * speed;
+        targetAngularSpeed = -Input.GetAxisRaw("Horizontal2") * angularSpeed;
+
+        // Log the position of the robot's base (optional for debugging)
+        Vector3 basePosition = robotBase.transform.position;
+        Quaternion baseRotation = robotBase.transform.rotation; // Get the rotation of the robot's base
+        Debug.Log($"Robot Base Position: {basePosition}");
+
+        // Log the positions of the points
+        Vector3 point2Position = point2.position;
+        Debug.Log($"Point2 Position: {point2Position}");
+
+        // Publish pose message with position and orientation
+        PublishPoseMessage(basePosition, baseRotation);
+    }
+
+    void FixedUpdate()
+    {
+        wheelController.SetRobotVelocity(targetLinearSpeed, targetAngularSpeed);
+    }
+
+    void PublishPoseMessage(Vector3 basePosition, Quaternion baseRotation)
+    {
+        PoseStampedMsg poseMessage = new PoseStampedMsg
+        {
+            header = new HeaderMsg
+            {
+                stamp = new TimeMsg
+                {
+                    sec = (int)Time.time,
+                    nanosec = (uint)((Time.time - Mathf.Floor(Time.time)) * 1e9)
+                },
+                frame_id = "base_link" // Or any other frame you are using
+            },
+            pose = new PoseMsg
+            {
+                position = new PointMsg { x = basePosition.x, y = basePosition.y, z = basePosition.z },
+                orientation = new QuaternionMsg
+                {
+                    x = baseRotation.x,
+                    y = baseRotation.y,
+                    z = baseRotation.z,
+                    w = baseRotation.w
+                }
+            }
+        };
+
+        ros.Publish(poseTopic, poseMessage);
+    }
+}
--- Ros2RhinoUnity/Assets/Scripts/NewKeyboardControl.cs
+++ Ros2RhinoUnity/Assets/Scripts/NewKeyboardControl.cs
@@ -5,38 +5,48 @@ using RosMessageTypes.Geometry;
 public class NewKeyboardControl : MonoBehaviour
 {
     private ROSConnection ros;
-    public string cmdVelTopic = "cmd_vel"; // The ROS topic to publish twist messages
-    public float maxLinearSpeed = 2.0f; // Maximum linear speed in m/s
-    public float maxAngularSpeed = 1.0f; // Maximum angular speed in rad/s
-    private float linearSpeed = 0.0f; // Current linear speed
-    private float angularSpeed = 0.0f; // Current angular speed
+    public string poseTopic = "pose"; // The ROS topic to publish pose messages
+    public float positionSpeed = 2.0f; // Speed of position change
+    public float rotationSpeed = 1.0f; // Speed of rotation change
+    private Vector3 position = Vector3.zero; // Current position
+    private Quaternion rotation = Quaternion.identity; // Current rotation
 
     void Start()
     {
         ros = ROSConnection.GetOrCreateInstance();
-        ros.RegisterPublisher<TwistMsg>(cmdVelTopic);
+        ros.RegisterPublisher<PoseMsg>(poseTopic);
     }
 
     void Update()
     {
         HandleKeyboardInput();
-        PublishTwistMessage();
+        PublishPoseMessage();
     }
 
     void HandleKeyboardInput()
     {
-        linearSpeed = Input.GetAxis("Vertical") * maxLinearSpeed;
-        angularSpeed = -Input.GetAxis("Horizontal") * maxAngularSpeed; // Adjust for negative input for correct direction
+        // Update position based on WASD keys
+        position += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * positionSpeed * Time.deltaTime;
+
+        // Update rotation based on QE keys
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rotation = Quaternion.Euler(rotation.eulerAngles + new Vector3(0, -rotationSpeed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            rotation = Quaternion.Euler(rotation.eulerAngles + new Vector3(0, rotationSpeed * Time.deltaTime, 0));
+        }
     }
 
-    void PublishTwistMessage()
+    void PublishPoseMessage()
     {
-        TwistMsg twistMessage = new TwistMsg
+        PoseMsg poseMessage = new PoseMsg
         {
-            linear = new Vector3Msg { x = linearSpeed, y = 0, z = 0 },
-            angular = new Vector3Msg { x = 0, y = 0, z = angularSpeed }
+            position = new PointMsg { x = position.x, y = position.y, z = position.z },
+            orientation = new QuaternionMsg { x = rotation.x, y = rotation.y, z = rotation.z, w = rotation.w }
         };
 
-        ros.Publish(cmdVelTopic, twistMessage);
+        ros.Publish(poseTopic, poseMessage);
     }
 }
